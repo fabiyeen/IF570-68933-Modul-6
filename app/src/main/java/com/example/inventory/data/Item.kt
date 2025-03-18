e548497551b2ce1505b50a126d8318a896ab13d0
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-03-19 00:06:52 +0700

    step 5 3

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -17,9 +17,11 @@
 package com.example.inventory.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "items")
 data class Item(
+    @PrimaryKey
     val id: Int = 0,
     val name: String,
     val price: Double,
